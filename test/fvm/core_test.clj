a86d864b64809861372be51f5398e95d91cc20d7
Divyansh Prakash <divyansh.prakash@yahoo.com>
2020-05-28 01:41:27 +0530

    Refactor tests

--- test/fvm/core_test.clj
+++ test/fvm/core_test.clj
@@ -11,10 +11,7 @@
 
 (deftest interpreter-test
   (testing "self tail recursive ops are jitted"
-    (let [range-script [{:op :requires
-                         :value ["lib/std.edn"]}
-
-                        {:op :defop
+    (let [range-script [{:op :defop
                          :name :range
                          :value [{:op :push
                                   :value 0}
@@ -23,7 +20,10 @@
                                          {:op :pop}]
                                   :else [{:op :pop}
                                          {:op :dup}
-                                         {:op :dec}
+                                         {:op :push
+                                          :value 1}
+                                         {:op :swap}
+                                         {:op :sub}
                                          {:op :range}]}]}]
 
           run-interpreted
@@ -34,8 +34,7 @@
                             {:op :range}])})
 
           test-interpreted-op #(:stack (run-interpreted %))
-          N 100
-          final-state (run-interpreted N)
+          final-state (run-interpreted 100)
           compiled-op (-> final-state :ops :range :compiled-trace)
           test-compiled-op (fn [n]
                              (-> {:stack [n]
@@ -50,8 +49,8 @@
                (test-compiled-op 200))))
 
       (testing "performance"
-        (is (<= (timing (test-compiled-op 300))
-                (timing (test-interpreted-op 300)))))))
+        (is (< (timing (test-compiled-op 300))
+               (timing (test-interpreted-op 300)))))))
 
   (testing "non-tail recursive ops are excluded from jit"
     (let [fact-script [{:op :requires
@@ -71,13 +70,12 @@
                                         {:op :dec}
                                         {:op :fact}
                                         {:op :mul}]}]}]
-          N 100
           run-fact #(fvm/interpret
                      {:code (concat fact-script
                                     [{:op :push
                                       :value %}
                                      {:op :fact}])})
-          final-state (run-fact N)
+          final-state (run-fact 100)
           fact-info (-> final-state :ops :fact)]      
       (is (:dont-jit? fact-info))
 
