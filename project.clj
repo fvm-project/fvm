65760b535cb3177ca276b9ebf006b70a10278d9e
Divyansh Prakash <divyansh.prakash@yahoo.com>
2020-06-02 16:12:47 +0530

    Revert due to bug introduced in 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fvm-project/fvm "0.1.0"
+(defproject fvm-project/fvm "0.1.2"
   :description "fvm is a Clojure library for writing self-optimizing interpreters"
   :url "https://github.com/fvm-project/fvm"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
